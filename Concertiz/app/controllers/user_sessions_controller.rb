08817a4d7ff3e05817870360549e813e865144f2
Auriles El Haddad <auriles0404@gmail.com>
2016-11-21 15:02:40 +0100

    Editing pages register + login ( fonctionnelle )

--- Concertiz/app/controllers/user_sessions_controller.rb
+++ Concertiz/app/controllers/user_sessions_controller.rb
@@ -5,9 +5,14 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:users, notice: 'Login successful')
+    u = User.where(email: params[:email]).first
+    p u
+    p login(params[:email], params[:password])
+    if login(params[:email], params[:password])
+      p "coucou"
+      redirect_to(:users, notice: 'Login successful')
     else
+      p "else"
       flash.now[:alert] = 'Login failed'
       render action: 'new'
     end
